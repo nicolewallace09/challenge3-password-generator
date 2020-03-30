deb45d9ec18a27033704fc62a8f2df43cc7f3ef5
nicolewallace09 <nicole.elisaw@gmail.com>
2020-03-29 23:31:07 -0700

    java/read me

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # challenge3-password-generator
 Challenge 3 Password Generator 
+
+## Criteria 
+Password length must be between 8 to 128 characters 
+Choose lowercase, uppercase, numeric, and/or speical characters
+
+## Screenshot
+<img src="assets/images/pass-gen-pic.png">
+
+## URL
+Github
+https://github.com/nicolewallace09/challenge3-password-generator
+
+Live
+https://nicolewallace09.github.io/challenge3-password-generator/
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,53 @@
-// Assignment code her e
+var randomStr = "";
+var specialCharacter = "!@#$%^&*()+[]{}`~ ";
+var number = " 0123456789 ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+console.log (specialCharacter, number, lowerCase, upperCase); 
+
+var generatePassword = function() {
+
+    // Password length between 8 - 128 characters 
+    var passwordLength = parseInt(window.prompt("Choose a password length between 8 and 128 characters"));
+    var passwordStr = "";
+
+    if (passwordLength  >= 8 && passwordLength < 128) {
+        // Choose lowercase, uppercase, numeric, and/or special characters 
+         var lowCasePrompt = window.prompt("Do you want to include lowercase letters in your password? Yes or No.").toLowerCase();
+            if (lowCasePrompt === "yes" || lowCasePrompt === "YES") {
+                randomStr = randomStr + lowerCase;
+                console.log(randomStr);
+                }
+
+
+     var upCasePrompt = window.prompt("Do you want to include uppercase letters in your password? Yes or No.").toLowerCase();
+        if (upCasePrompt === "yes" ||  upCasePrompt === "YES") {
+        randomStr = randomStr + upperCase;
+        console.log(upperCase);
+        }
+
+    var specialPrompt = window.prompt("Do you want to include special characters in your password? Yes or No.").toLowerCase();
+        if (specialPrompt === "yes" || specialPrompt === "YES") {
+        randomStr = randomStr + specialCharacter;
+        console.log(specialCharacter);
+        }
+
+    var numPrompt = window.prompt("Do you want to include numbers in your password? Yes or No.").toLowerCase();
+        if (numPrompt === "yes" || numPrompt === "YES") {
+        randomStr = randomStr + number;   
+        }
+    }
+
+// Generate random 
+var newPassword = "";
+for (var i = 0; i < passwordLength; i++) {
+    passwordStr += randomStr.charAt[Math.floor(Math.random() * randomStr.length)];
+    console.log(newPassword);
+    window.alert(newPassword); 
+} 
+
+};
 
 // Get references to the #generate element 
 var generateBtn = document.querySelector('#generate');
@@ -13,4 +62,3 @@ function writePassword () {
 
 // Add event listener to generate button 
 generateBtn.addEventListener("click", writePassword);
-
