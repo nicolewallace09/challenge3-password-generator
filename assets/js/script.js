87a4fc85c02bac60db25bef28a8a7259f743f5f2
nicolewallace09 <nicole.elisaw@gmail.com>
2020-08-19 16:16:04 -0700

    may have fixed issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,47 +15,53 @@ var generatePassword = function() {
     var passwordLength = parseInt(window.prompt("Choose a password length between 8 and 128 characters"));
 
     if (passwordLength  >= 8 && passwordLength <= 128) {
+        console.log(passwordLength)
         // Choose lowercase, uppercase, numeric, and/or special characters 
         // lowercase
         var lowCasePrompt = window.prompt("Do you want to include lowercase letters in your password? Yes or No.");
             if (lowCasePrompt === "yes" || lowCasePrompt === "YES") {
                 randomStr = randomStr + lowerCase;
+                console.log(randomStr)
             }
 
         // uppercase 
         var upCasePrompt = window.prompt("Do you want to include uppercase letters in your password? Yes or No.");
             if (upCasePrompt === "yes" ||  upCasePrompt === "YES") {
                 randomStr = randomStr + upperCase;
+                console.log(randomStr)
             }
 
         // special char
         var specialPrompt = window.prompt("Do you want to include special characters in your password? Yes or No.");
             if (specialPrompt === "yes" || specialPrompt === "YES") {
                 randomStr = randomStr + specialCharacter;
-            }
+                console.log(randomStr)
+            } 
 
         // numbers 
         var numPrompt = window.prompt("Do you want to include numbers in your password? Yes or No.");
             if (numPrompt === "yes" || numPrompt === "YES") {
-                randomStr = randomStr + number;   
+                randomStr = randomStr + number;  
+                console.log(randomStr) 
             }
-    }
-    else { 
-        window.alert("Invalid. Please try again!"); 
-        return;
-    }
-
-    if (lowCasePrompt === false && upCasePrompt === false && specialPrompt === false && numPrompt === false) {
-        window.alert('Invalid. Must choose a character');
-        return; 
-    }
-
-    for (var i = 0; i < passwordLength; i++) {
-        passwordStr += randomStr.charAt(Math.floor(Math.random() * randomStr.length));
-    } 
-        var finalPassword = passwordStr;
-        return finalPassword; 
+        }
+        else { 
+            window.alert("Invalid. Please try again!"); 
     };
+
+    var charTypes = lowCasePrompt + upCasePrompt + specialPrompt + numPrompt
+
+    if (!charTypes) {
+        return; 
+    } else {
+
+        for (var i = 0; i < passwordLength; i++) {
+            passwordStr += randomStr.charAt(Math.floor(Math.random() * randomStr.length));
+        } 
+            var finalPassword = passwordStr;
+            return finalPassword; 
+    }
+};
 
 // Get references to the #generate element 
 var generateBtn = document.querySelector('#generate');
